0a08d42cf23e964dedf1b2c3b2cf492fe2a91d50
15GaelNG <gaelgutierrez15@gmail.com>
2025-07-30 16:31:13 -0600

    Se actualiza el main e index

--- src/js/main.js
+++ src/js/main.js
@@ -7,48 +7,88 @@ import { initFloatingButtons } from "./components/floating-buttons.js";
 import { initSlide2Carousel } from "./sections/slide2.js";
 import { initNavbarSlide1, initPlantarArbolButton } from "./components/navbar-slide1.js";
 
-// Configuración de componentes
-const COMPONENTS = [
-  {
-    id: "slide1",
-    html: "html/sections/slide1.html",
-    css: "css/sections/slide1.css",
-    init: () => {
-      enableSlide1ScrollButton();
-      initNavbarSlide1();
-      initPlantarArbolButton();
-    }
-  },
-  {
-    id: "slide2", 
-    html: "html/sections/slide2.html",
-    css: "css/sections/slide2.css",
-    init: initSlide2Carousel
-  },
-  {
-    id: "slide3",
-    html: "html/sections/slide3.html",
-    css: "css/sections/slide3.css",
-    init: null // Sin funcionalidad JavaScript específica por ahora
-  },
-  {
-    id: "floating-buttons",
-    html: "html/components/floating-buttons.html", 
-    css: "css/components/floating-buttons.css",
-    init: initFloatingButtons
-  }
-];
-
 // Carga optimizada de componentes
 async function initComponents() {
-  const promises = COMPONENTS.map(({ id, html, css, init }) => 
-    loadComponent(id, html, css, init)
-  );
-  
   try {
-    await Promise.all(promises);
+    // Cargar Slide 1
+    await loadComponent(
+      "slide1",
+      "html/sections/slide1.html",
+      "css/sections/slide1.css",
+      () => {
+        console.log("Slide 1 cargado");
+        enableSlide1ScrollButton();
+        initNavbarSlide1();
+        initPlantarArbolButton();
+      }
+    );
+
+    // Cargar Slide 2
+    await loadComponent(
+      "slide2",
+      "html/sections/slide2.html",
+      "css/sections/slide2.css",
+      () => {
+        console.log("Slide 2 cargado");
+        initSlide2Carousel();
+      }
+    );
+
+    // Cargar Slide 3
+    await loadComponent(
+      "slide3",
+      "html/sections/slide3.html",
+      "css/sections/slide3.css",
+      () => {
+        console.log("Slide 3 cargado");
+      }
+    );
+
+    // Cargar Slide 4
+    await loadComponent(
+      "slide4",
+      "html/sections/slide4.html",
+      "css/sections/slide4.css",
+      () => {
+        console.log("Slide 4 cargado");
+      }
+    );
+
+    // Cargar Slide 5
+    await loadComponent(
+      "slide5",
+      "html/sections/slide5.html",
+      "css/sections/slide5.css",
+      () => {
+        console.log("Slide 5 cargado");
+      }
+    );
+
+    // Cargar Slide 6
+    await loadComponent(
+      "slide6",
+      "html/sections/slide6.html",
+      "css/sections/slide6.css",
+      () => {
+        console.log("Slide 6 cargado");
+      }
+    );
+
+    // Cargar Floating Buttons
+    await loadComponent(
+      "floating-buttons",
+      "html/components/floating-buttons.html",
+      "css/components/floating-buttons.css",
+      () => {
+        console.log("Floating buttons cargados");
+        initFloatingButtons();
+      }
+    );
+
+    // Habilitar scroll behavior
     enableScrollBehavior();
     console.log("Todos los componentes cargados exitosamente");
+
   } catch (error) {
     console.error("Error cargando componentes:", error);
   }
